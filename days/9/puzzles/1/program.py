650efbc24accda5cd3d0bee9a2236021a6b735b3
Christopher Wojcik <hello@chriswojcik.net>
2019-12-09 23:32:37 -0500

    cleaner? intcode computer

--- days/9/puzzles/1/program.py
+++ days/9/puzzles/1/program.py
@@ -20,11 +20,26 @@ class Computer():
     self._program = program
     self.reset_memory()
 
-  def read(self, pointer):
+  def _modeless_read(self, pointer):
     return self._memory.get(int(pointer)) or '0'
 
-  def write(self, pointer, value):
-    self._memory[int(pointer)] = str(value)
+  def read(self, mode, pointer):
+    if mode == POSITION_MODE:
+      position = int(self._modeless_read(pointer))
+      return self._modeless_read(position)
+    elif mode == IMMEDIATE_MODE:
+      return self._modeless_read(pointer)
+    elif mode == RELATIVE_MODE:
+      offset = int(self._modeless_read(pointer))
+      return self._modeless_read(offset + self._relative_base)
+
+  def write(self, mode, pointer, value):
+    if mode == POSITION_MODE or mode == IMMEDIATE_MODE:
+      address = int(self._modeless_read(pointer))
+    if mode == RELATIVE_MODE:
+      address = int(self._modeless_read(pointer)) + self._relative_base
+
+    self._memory[address] = str(value)
 
   def reset_memory(self):
     instructions = self._program.split(',')
@@ -36,29 +51,13 @@ class Computer():
     self._memory = memory
     self._relative_base = 0
 
-  def _read_by_parameter(self, mode, pointer):
-    if mode == POSITION_MODE:
-      position = int(self.read(pointer))
-      return int(self.read(position))
-    elif mode == IMMEDIATE_MODE:
-      return int(self.read(pointer))
-    elif mode == RELATIVE_MODE:
-      offset = int(self.read(pointer))
-      return int(self.read(offset + self._relative_base))
-
-  def _get_write_address(self, mode, pointer):
-    if mode == POSITION_MODE or mode == IMMEDIATE_MODE:
-      return int(self.read(pointer))
-    if mode == RELATIVE_MODE:
-      return int(self.read(pointer)) + self._relative_base
-
   def run(self, inputs = []):
     i = 0
     inputs_pointer = 0
     outputs = []
 
     while True:
-      instruction = self.read(i)
+      instruction = self._modeless_read(i)
       op_code = int(instruction[-2:])
       param_modes = instruction[:-2][::-1]
       param_1_mode = int(param_modes[0]) if len(param_modes) else DEFAULT_MODE
@@ -66,72 +65,66 @@ class Computer():
       param_3_mode = int(param_modes[2]) if len(param_modes) > 2 else DEFAULT_MODE
 
       if op_code == ADDITION_OP:
-        param_1 = self._read_by_parameter(param_1_mode, i + 1)
-        param_2 = self._read_by_parameter(param_2_mode, i + 2)
-        write_address = self._get_write_address(param_3_mode, i + 3)
+        value_1 = self.read(param_1_mode, i + 1)
+        value_2 = self.read(param_2_mode, i + 2)
 
-        self.write(write_address, param_1 + param_2)
+        self.write(param_3_mode, i + 3, int(value_1) + int(value_2))
 
         i += 4
       elif op_code == MULTIPLY_OP:
-        param_1 = self._read_by_parameter(param_1_mode, i + 1)
-        param_2 = self._read_by_parameter(param_2_mode, i + 2)
-        write_address = self._get_write_address(param_3_mode, i + 3)
+        value_1 = self.read(param_1_mode, i + 1)
+        value_2 = self.read(param_2_mode, i + 2)
 
-        self.write(write_address, param_1 * param_2)
+        self.write(param_3_mode, i + 3, int(value_1) * int(value_2))
 
         i += 4
       elif op_code == INPUT_OP:
         value = inputs[inputs_pointer]
         inputs_pointer += 1
-        write_address = self._get_write_address(param_1_mode, i + 1)
 
-        self.write(write_address, value)
+        self.write(param_1_mode, i + 1, value)
 
         i += 2
       elif op_code == OUTPUT_OP:
-        param_1 = self._read_by_parameter(param_1_mode, i + 1)
-        value = str(param_1)
-
+        value = self.read(param_1_mode, i + 1)
         outputs.append(value)
+        print(value)
 
         i += 2
       elif op_code == JUMP_IF_TRUE_OP:
-        param_1 = self._read_by_parameter(param_1_mode, i + 1)
-        param_2 = self._read_by_parameter(param_2_mode, i + 2)
+        value_1 = self.read(param_1_mode, i + 1)
+        value_2 = self.read(param_2_mode, i + 2)
 
-        if param_1 != 0:
-          i = param_2
+        if int(value_1) != 0:
+          i = int(value_2)
         else:
           i += 3
       elif op_code == JUMP_IF_FALSE_OP:
-        param_1 = self._read_by_parameter(param_1_mode, i + 1)
-        param_2 = self._read_by_parameter(param_2_mode, i + 2)
+        value_1 = self.read(param_1_mode, i + 1)
+        value_2 = self.read(param_2_mode, i + 2)
 
-        if param_1 == 0:
-          i = param_2
+        if int(value_1) == 0:
+          i = int(value_2)
         else:
           i += 3
       elif op_code == LESS_THAN_OP:
-        param_1 = self._read_by_parameter(param_1_mode, i + 1)
-        param_2 = self._read_by_parameter(param_2_mode, i + 2)
-        write_address = self._get_write_address(param_3_mode, i + 3)
+        value_1 = self.read(param_1_mode, i + 1)
+        value_2 = self.read(param_2_mode, i + 2)
 
-        self.write(write_address, '1' if param_1 < param_2 else '0')
+        self.write(param_3_mode, i + 3, '1' if int(value_1) < int(value_2) else '0')
 
         i += 4
       elif op_code == EQUALS_OP:
-        param_1 = self._read_by_parameter(param_1_mode, i + 1)
-        param_2 = self._read_by_parameter(param_2_mode, i + 2)
-        write_address = self._get_write_address(param_3_mode, i + 3)
+        value_1 = self.read(param_1_mode, i + 1)
+        value_2 = self.read(param_2_mode, i + 2)
 
-        self.write(write_address, '1' if param_1 == param_2 else '0')
+        self.write(param_3_mode, i + 3, '1' if value_1 == value_2 else '0')
 
         i += 4
       elif op_code == RELATIVE_BASE_OFFST_OP:
-        param_1 = self._read_by_parameter(param_1_mode, i + 1)
+        value = self.read(param_1_mode, i + 1)
 
-        self._relative_base += param_1
+        self._relative_base += int(value)
 
         i += 2
       elif op_code == HALT_OP:
@@ -140,4 +133,4 @@ class Computer():
 program = sys.stdin.read()
 computer = Computer(program)
 
-print(computer.run('1')[0])
+computer.run('1')
