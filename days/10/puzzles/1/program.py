c0bccce36a8034978d6e324038e72e16055c0a83
Christopher Wojcik <hello@chriswojcik.net>
2019-12-10 23:20:08 -0500

    using angles for day 10 part 1 is n^2, don't need to know what is blocking what

--- days/10/puzzles/1/program.py
+++ days/10/puzzles/1/program.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 x = 0
 y = 0
@@ -15,42 +16,25 @@ for line in sys.stdin:
 
   y += 1
 
-def collinear(p1, p2, p3):
+def angle_between(p1, p2):
   x1,y1 = p1
   x2,y2 = p2
-  x3,y3 = p3
 
-  return (x1 * (y2 - y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2)) == 0
+  return math.atan2(x2 - x1, y2 - y1) # flip x and y because grid is rotated
 
-def between(p1, p2, p3):
-  x1,y1 = p1
-  x2,y2 = p2
-  x3,y3 = p3
-
-  if x1 != x2:
-    return x1 <= x3 <= x2 or x2 <= x3 <= x1
-  else:
-    return y1 <= y3 <= y2 or y2 <= y3 <= y1
-
-visible_asteroids = {}
+unique_line_of_sight_angles = {}
 monitoring_station_location = None
 max_visible_asteroids = 0
 
 for p1 in asteroids:
-  visible_asteroids[p1] = set()
+  unique_line_of_sight_angles[p1] = set()
 
   for p2 in asteroids:
     if p2 != p1:
-      has_line_of_sight = True
-      for p3 in asteroids:
-        if p1 != p2 and p2 != p3 and p3 != p1:
-          if collinear(p1, p2, p3) and between(p1, p2, p3):
-            has_line_of_sight = False
-            break
-      if has_line_of_sight:
-        visible_asteroids[p1].add(p2)
+      theta = angle_between(p1, p2)
+      unique_line_of_sight_angles[p1].add(theta)
 
-  num_visible_asteroids = len(visible_asteroids[p1])
+  num_visible_asteroids = len(unique_line_of_sight_angles[p1])
 
   if num_visible_asteroids > max_visible_asteroids:
     max_visible_asteroids = num_visible_asteroids
